b86bf44e95e5bf74e0caeec920562adf2b55be61
amodsachintha <amod077@gmail.com>
2019-10-28 14:53:38 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,7 +44,7 @@
                         </div>
                         <div class="card-footer">
                             <div class="progress" style="height: 30px;">
-                                <div class="progress-bar {{$row['completePercentage'] === 100 ? 'bg-success' : 'progress-bar-striped progress-bar-animated bg-dark'}}" role="progressbar" style="width: {{$row['completePercentage']}}%;"
+                                <div class="progress-bar progress-bar-striped {{$row['completePercentage'] === 100 ? 'bg-success' : 'progress-bar-animated bg-dark'}}" role="progressbar" style="width: {{$row['completePercentage']}}%;"
                                      aria-valuenow="{{$row['completePercentage']}}" aria-valuemin="0" aria-valuemax="100">
                                     <strong>{{$row['completePercentage']}}%</strong>
                                 </div>
