53bc2b2e8bed4edb11b73cff942a5473ee059a42
Amod Sachintha <amod077@gmail.com>
2025-08-15 11:45:58 +0000

    fix route service provider constant

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,6 +29,13 @@ class RouteServiceProvider extends ServiceProvider
     }
 
     /**
+     * The path to the "home" route for your application.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
+    /**
      * Define the routes for the application.
      *
      * @return void
